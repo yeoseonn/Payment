a1779618513bbd4bf55c6c6a7106505bd47638ac
yeosoennn <yys265@naver.com>
2020-06-06 09:26:38 +0900

    #1 모델 설계 및 Mybatis Setting

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE pay_cur_info(
+id INT(11) auto_increment,
+payment_id VARCHAR(20) NOT NULL,
+pay_type VARCHAR(10) NOT NULL,
+cur_vat INT(11),
+cur_pay_amount INT(11),
+plan_month VARCHAR(2),
+created DATETIME DEFAULT CURRENT_TIMESTAMP ,
+updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE pay_req_info(
+id INT(11) auto_increment,
+payment_id VARCHAR(20) NOT NULL,
+origin_payment_id VARCHAR(20),
+pay_type VARCHAR(10) NOT NULL,
+vat INT(11),
+pay_amount INT(11),
+plan_month VARCHAR(2),
+created DATETIME DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY(id)
+);
